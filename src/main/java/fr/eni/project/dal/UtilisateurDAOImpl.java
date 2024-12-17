20ed31c2dd2c70188775ff8571ac80428e46ff20
Eva FILLIAUDEAU <eva.filliaudeau2024@campus-eni.fr>
2024-12-17 15:07:22 +0100

    developpement filtres en cours

--- src/main/java/fr/eni/project/dal/UtilisateurDAOImpl.java
+++ src/main/java/fr/eni/project/dal/UtilisateurDAOImpl.java
@@ -7,7 +7,6 @@ import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Repository;
 
 import fr.eni.project.bo.Utilisateur;
