ede391fcdbb7eeb1a51cbe732c515c17ab411424
Océane <oceane.brosseau2024@campus-eni.fr>
2024-12-18 12:11:55 +0100

    maj vente article

--- src/main/java/fr/eni/project/controller/EnchereController.java
+++ src/main/java/fr/eni/project/controller/EnchereController.java
@@ -2,21 +2,25 @@ package fr.eni.project.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import fr.eni.project.bll.EnchereService;
+import fr.eni.project.bll.UtilisateurService;
 import fr.eni.project.bo.Enchere;
+import fr.eni.project.bo.Utilisateur;
 
 @Controller
 @RequestMapping
 //@SessionAttributes({"utilisateur-profile"})
 public class EnchereController {
 
-
 	private EnchereService enchereService;
+	@Autowired
+	private UtilisateurService addressUser;
 
 	public EnchereController(EnchereService enchereService) {
 		this.enchereService = enchereService;
@@ -27,14 +31,14 @@ public class EnchereController {
 		List<Enchere> encheres = this.enchereService.afficherEncheres();
 		model.addAttribute("encheres", encheres);
 		return "index";
-		}
-	
-	@GetMapping("/sell-article")
-	public String afficherVendreArticle(Model model) {
-		model.addAttribute("enchere", new Enchere());
-		return "sell-article";
 	}
-	
-	
-	
+
+	/*
+	 * @GetMapping("/sell-article") public String afficherVendreArticle(Model model)
+	 * { String pseudoUser = addressUser.afficherUtilisateurParPseudo(); Utilisateur
+	 * user = addressUser.afficherUtilisateurParPseudo(noUtilisateur);
+	 * user.getRue(); user.getCodePostal(); user.getVille();
+	 * model.addAttribute("utilisateur", user); return "sell-article"; }
+	 */
+
 }
